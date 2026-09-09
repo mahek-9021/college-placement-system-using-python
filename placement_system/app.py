e9ded8a0cbbee01e2e963128a2b804939910e965
Shaikh mahek <shaikmahek@gmail.com>
2026-09-09 10:22:44 +0530

    add gunicorn

--- placement_system/app.py
+++ placement_system/app.py
@@ -500,3 +500,6 @@ if __name__ == "__main__":
     else:
         init_db()  # safe: CREATE TABLE IF NOT EXISTS
     app.run(debug=True, host="0.0.0.0", port=5000)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
